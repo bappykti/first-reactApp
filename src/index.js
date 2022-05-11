699d4ed78e00555486ac96e79c008bf982dea17a
bappykti <bappykti@gmail.com>
2022-05-11 09:07:19 +0600

    add css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// import './index.css'
+const todoTitel = "Call family";
+const todoDesc = "Make a copy";
+const date = new Date();
+const dateName = date.getDate();
+const monthName = date.getMonth();
+const yearName = date.getFullYear();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const headingStyle = {
+  color:"red",
+  fontSize:"3rem",
+  textAlign: "center",
+  padding: "15px"
+
+}
+ReactDOM.render(
+  <div>
+  <h1 className='headingStyle largeText'>"Hello world of React"</h1>
+  <h3>{todoTitel}</h3>
+  <h3>{todoDesc}</h3>
+  <h3>{dateName + " " + monthName + " " + yearName}</h3>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+  <p>19/05/2022</p>
+
+
+
+  </div>,
+  // <h1>My First react App</h1>, // render allow only one elements at a time, for displaying many elements ata time we use div, within div we integrate many elements.
+  
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
