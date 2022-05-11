98536f8b4ec70ccf4ff797e2233fa21fbdf54a91
bappykti <bappykti@gmail.com>
2022-05-11 11:56:59 +0600

    little chjange

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import './index.css'
-const todoTitel = "Call family";
-const todoDesc = "Make a copy";
+const todoTitle = "Call family";
+const todoDesc = "Lorem Ipsum is simply dummy text of the printing and Lorem Ipsum is simply dummy text of the printing";
 const date = new Date();
-const dateName = date.getDate();
+const dateNumber = date.getDate();
 const monthName = date.getMonth();
 const yearName = date.getFullYear();
 
-const headingStyle = {
-  color:"red",
-  fontSize:"3rem",
-  textAlign: "center",
-  padding: "15px"
-
-}
+// const headingStyle = {
+//   color:"red",
+//   fontSize:"3rem",
+//   textAlign: "center",
+//   padding: "15px"
+// }
 ReactDOM.render(
   <div>
-  <h1 className='headingStyle largeText'>"Hello world of React"</h1>
-  <h3>{todoTitel}</h3>
-  <h3>{todoDesc}</h3>
-  <h3>{dateName + " " + monthName + " " + yearName}</h3>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-  <p>19/05/2022</p>
-
-
-
+    <h1 className="headingStyle largeText">"Hello world of React"</h1>
+    <div className="card">
+      <h4 className="cardTitle">{todoTitle}</h4>
+      <p className="cardDesc">{todoDesc}</p>
+      <p className='cardFooter'>{dateNumber + " " + monthName + " " + yearName}</p> 
+    </div>
   </div>,
   // <h1>My First react App</h1>, // render allow only one elements at a time, for displaying many elements ata time we use div, within div we integrate many elements.
-  
-  document.getElementById('root')
+
+  document.getElementById("root")
 );
