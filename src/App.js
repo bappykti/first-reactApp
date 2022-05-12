5a62b6825f8c7ae58314aef19cf84ecffc59acf9
bappykti <bappykti@gmail.com>
2022-05-12 09:59:50 +0600

    use mapping mathod

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Card from "./components/Card";
+import Data from "./data.json";
 
 function App(){
+
+    // let items = [];
+    // for(let x=0; x<Data.length; x++){
+    //     items.push(<Card titleText = {Data[x].title} descText = {Data[x].desc} />)
+    // }
+
+    // let items = [];
+    // items = Data.map((item)=> <Card titleText={item.title} descText={item.desc} />)
+
     return <div>
         <h1 className="headingStyle"> Todo App</h1>
-        <Card titleText = "Call Mother" descText = "This is Desc 1" />
-        <Card titleText = "Call Father" descText = "This is Desc 2" />
-        <Card titleText = "Call Brother" descText = "This is Desc 3" />
-        <Card titleText = "Call Wife" descText = "This is Desc 4" />
-        <Card titleText = "Call Uncle" descText = "This is Desc 5" />
-        
+        {Data.map((item, index)=> <Card key={index} titleText={item.title} descText={item.desc} />)}
+        {/* <Card titleText = {Data[0].title} descText = {Data[0].desc} /> */}
+        {/* <Card titleText = {Data[1].title} descText = {Data[1].desc} />
+        <Card titleText = {Data[2].title} descText = {Data[2].desc} />
+        <Card titleText = {Data[3].title} descText = {Data[3].desc} />         */}
     </div>
    
 }
