710d0adb3cbf99a7068a80645a6eb6796290f231
bappykti <bappykti@gmail.com>
2022-05-14 10:41:55 +0600

    nested mapping

---
+++ src/App2.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const users = [
+    {
+        fullName : "Kazi Tanvirul Islam",
+        age : 32,
+        phones : [
+            {home : "0721-770879"},
+            {office : "01728-031062"}
+        ],
+    },
+    {
+        fullName : "Kazi Raihanul Islam",
+        age : 26,
+        phones : [
+            {home : "0721-770879"},
+            {office : "01724-113114"}
+        ],
+    },
+];
+
+export default function App2(){
+    return(
+        <div>
+            <h1>Nested Lists</h1>
+            {
+                users.map((user, index) => (
+                    <article key = {index}>
+                        <h3>Full Name : {user.fullName}</h3>
+                        <p>Age : {user.age}</p>
+                        {
+                            user.phones.map((phone, index) => 
+                                (<div key = {index}>
+                                    <p>{phone.home}</p>
+                                    <p>{phone.office}</p>
+                                </div>))
+                        }
+                    </article>
+                ) )
+            }
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 // import App1 from "./App1";
+import App2 from "./App2";
 // import './index.css'
 
 
@@ -11,5 +12,5 @@ import App from "./App";
 //   textAlign: "center",
 //   padding: "15px"
 // }
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App2 />, document.getElementById("root"));
 // ReactDOM.render(<App1 />, document.getElementById("root"));
