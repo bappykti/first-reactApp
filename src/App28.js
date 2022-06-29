c9a3b007d0bac17b4c1af6ecc66e57981a3a7e8c
bappykti <bappykti@gmail.com>
2022-06-29 17:59:20 +0600

    callback function

--- src/App28.js
+++ src/App28.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+// React Memo
+// callback function
+import React, { useCallback, useState } from 'react';
 import Message from './React_Memo/Message';
 
 
 const App28 = () => {
     const [count, setCount] = useState(0);
+    const [toggle, setToggle] = useState(false);
     console.log("Rendering Hello world App");
+    const handleIncrementMessage = useCallback( () => {
+      setCount(count +1);
+    },[count]);
   return (
     <div>
+      {toggle ? "On" : "Off"}
+      <button onClick={()=>{setToggle(!toggle)}}>Toggle</button>
         <h1>Count : {count}</h1>
         <button onClick={()=>{
             setCount((count) => count +1);
         }}>Increment</button>
-        <Message numberOfMessages = {count} />
+        <Message numberOfMessages = {count} onHandleIncrement={handleIncrementMessage}/>
     </div>
     
   )
--- src/React_Memo/Message.js
+++ src/React_Memo/Message.js
@@ -1,10 +1,13 @@
+// React Memo
+// Callback function
 import React, { memo } from 'react';
 
-const Message = ({numberOfMessages}) => {
+const Message = ({numberOfMessages, onHandleIncrement}) => {
     console.log("Rendering Message");
   return (
     <div>
         <h2>Message {numberOfMessages} Count</h2>
+        <button onClick={onHandleIncrement}>Increment Message Number</button>
 
     </div>
   )
